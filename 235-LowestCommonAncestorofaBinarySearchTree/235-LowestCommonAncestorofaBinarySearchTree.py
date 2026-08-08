db93da4561bc88f333705eea5a11811712470c25
no0e <116623947+no0e@users.noreply.github.com>
2026-08-08 15:24:05 +0200

    File updated at 08/08/2026 15:24:05

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 08/08/2026 15:20:31
+# Last updated: 08/08/2026 15:24:05
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, x):
@@ -7,34 +7,20 @@
 6#         self.right = None
 7
 8class Solution(object):
-9    
-10    def contient_noeud(self, root, cible):
-11        if not root:
-12            return False
-13        if cible == root:
-14            return True
-15        
-16        
-17        gauche = self.contient_noeud(root.left, cible)
-18        droite = self.contient_noeud(root.right, cible)
-19
-20        return gauche or droite
-21        
-22    def lowestCommonAncestor(self, root, p, q):
-23        
-24        if not root or root == p or root == q:
-25            return root
-26
-27       
-28        p_gauche = self.contient_noeud(root.left, p)
-29        q_gauche = self.contient_noeud(root.left, q)
-30
-31        if p_gauche != q_gauche:
-32            return root
-33        else:
-34            if p_gauche:
-35                
-36                return self.lowestCommonAncestor(root.left, p, q)
-37            else: 
-38                
-39                return self.lowestCommonAncestor(root.right, p, q)
+9        
+10    def lowestCommonAncestor(self, root, p, q):
+11        
+12        if not root or root == p or root == q:
+13            return root
+14
+15       
+16        gauche = self.lowestCommonAncestor(root.left, p, q)
+17        droite = self.lowestCommonAncestor(root.right, p, q)
+18
+19        if gauche and droite:
+20            return root
+21
+22        return gauche or droite
+23       
+24
+25            
