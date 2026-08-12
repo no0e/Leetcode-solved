f39eae2c3a90fbc31452b552ac4d2e52a94fdfa1
no0e <116623947+no0e@users.noreply.github.com>
2026-08-12 11:47:18 +0200

    File updated at 12/08/2026 11:47:18

--- 621-TaskScheduler/621-TaskScheduler.py
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -1,4 +1,4 @@
-# Last updated: 12/08/2026 11:46:56
+# Last updated: 12/08/2026 11:47:18
 1from collections import Counter, deque
 2import heapq
 3
@@ -11,27 +11,25 @@
 10        """
 11        count = Counter(tasks) 
 12        
-13        # 1. On crée d'abord la liste classique
-14        tas = [-c for c in count.values()]
-15        # 2. On la transforme en tas sur place (ne rien assigner à une variable)
-16        heapq.heapify(tas)
-17
-18        file_attente = deque()
-19        t = 0
-20
-21        while tas or file_attente:
-22            t += 1
-23            
-24            if tas:
-25                # 3. On utilise bien la variable 'tas' ici
-26                c = heapq.heappop(tas) + 1
-27                
-28                if c != 0:
-29                    file_attente.append([c, t + n])
-30            
-31            if file_attente and file_attente[0][1] == t:
-32                tache_repose = file_attente.popleft()
-33                # 3. On utilise bien la variable 'tas' ici aussi
-34                heapq.heappush(tas, tache_repose[0])
-35                
-36        return t
+13        tas = [-c for c in count.values()]
+14        heapq.heapify(tas)
+15
+16        file_attente = deque()
+17        t = 0
+18
+19        while tas or file_attente:
+20            t += 1
+21            
+22            if tas:
+23                
+24                c = heapq.heappop(tas) + 1
+25                
+26                if c != 0:
+27                    file_attente.append([c, t + n])
+28            
+29            if file_attente and file_attente[0][1] == t:
+30                tache_repose = file_attente.popleft()
+31
+32                heapq.heappush(tas, tache_repose[0])
+33                
+34        return t
